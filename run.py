a44646fc94438c93a07705b2082a889af5b7f6ad
pal772 <pal722998@gmail.com>
2022-11-07 18:37:52 +0000

    Initial commit and added main functions

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def new_game():
+    pass
+
+def check_answer():
+    pass
+
+def display_score():
+    pass
+
+def play_again():
+    pass
+
