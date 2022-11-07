73f229176a4632efe4542e625dd0e09223759ccb
pal772 <pal722998@gmail.com>
2022-11-07 19:11:51 +0000

    Added some Questions and Answers to the Quiz game

--- run.py
+++ run.py
@@ -10,3 +10,11 @@ def display_score():
 def play_again():
     pass
 
+
+questions = { 
+"What is the name of the song that Queen Elsa sings as she builds her ice castle in the frozen movie?: ": "A",
+"How many films did Sean Connery play James Bond?: ": "C",
+"Which Indiana Jones movie was released back in 1984?: ": "D",
+"How many Lord of the Rings films are there?: ": "A",
+"What was the name of the actor who plays Jack Dawson in Titanic?: ": "B"
+}
